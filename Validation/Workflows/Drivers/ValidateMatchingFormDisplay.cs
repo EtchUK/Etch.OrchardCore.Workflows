c83713c45fda0c7d08c08bd7ad03ac6e0a056a5a
Peter Keating <peter@moov2.com>
2019-09-06 15:49:04 +0100

    Add workflow task for validating matching form values

---
+++ Validation/Workflows/Drivers/ValidateMatchingFormDisplay.cs
@@ -0,0 +1,25 @@
+﻿using Etch.OrchardCore.Workflows.Validation.Workflows.Activities;
+using Etch.OrchardCore.Workflows.Validation.Workflows.ViewModels;
+using OrchardCore.Workflows.Display;
+
+namespace Etch.OrchardCore.Workflows.Validation.Workflows.Drivers
+{
+    public class ValidateMatchingFormDisplay : ActivityDisplayDriver<ValidateMatchingTask, ValidateMatchingTaskViewModel>
+    {
+        #region Implementation
+
+        protected override void EditActivity(ValidateMatchingTask activity, ValidateMatchingTaskViewModel model)
+        {
+            model.ErrorMessage = activity.ErrorMessage;
+            model.ToValidate = activity.ToValidate;
+        }
+
+        protected override void UpdateActivity(ValidateMatchingTaskViewModel model, ValidateMatchingTask activity)
+        {
+            activity.ErrorMessage = model.ErrorMessage;
+            activity.ToValidate = model.ToValidate;
+        }
+
+        #endregion Implementation
+    }
+}
---
+++ Validation/Workflows/ViewModels/ValidateMatchingTaskViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Etch.OrchardCore.Workflows.Validation.Workflows.ViewModels
+{
+    public class ValidateMatchingTaskViewModel
+    {
+        public string ErrorMessage { get; set; }
+        public string ToValidate { get; set; }
+    }
+}
